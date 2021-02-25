ffa6ce4fddaac2bef5c4d431b9144874089bafae
Leo B <bernerdoodle@outlook.de>
2021-02-25 13:01:06 +0100

    1.0.5

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='friendlier-json',
+    version='1.0-pre-alpha',
+    packages=['friendlydb'],
+    url='https://github.com/py-alpha-wolf/friendlier-json',
+    license='MIT',
+    author='Leo B.',
+    author_email='bernerdoodle@outlook.de',
+    description='friendlier-json turns your .json into an Database :D'
+)
